a41cf9f0c59531bac61f87af37afe7041446c7e7
Jaewookahn1079 <ahnjaewook1079@gmail.com>
2025-10-17 16:35:20 +0900

    test

---
+++ js/intro.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.intro_btn').addEventListener('click', () => {
+        location.href = '/html/main.html';
+    })
+})
