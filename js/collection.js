bce0f24dd40285cec7e48a8bbd9c2c7c4457cfb0
phr106-cpu <phr106@gmail.com>
2025-10-21 12:08:32 +0900

    hr

--- js/collection.js
+++ js/collection.js
@@ -2,12 +2,10 @@ const viewBtns = document.querySelectorAll('.view_btns button');
 
 viewBtns.forEach(btn => {
   btn.addEventListener('click', () => {
-    // 모든 버튼 초기화
     viewBtns.forEach(b => {
       b.classList.remove('on');
     });
 
-    // 클릭한 버튼 활성화(on 이미지로 변경)
     btn.classList.add('on');
   });
 });
