f029702f75bc50185b1bd0d09ca7256fa4451add
hijisoo <ab0788@naver.com>
2025-10-21 19:00:07 +0900

    gsap

--- js/vision.js
+++ js/vision.js
@@ -1,26 +1,29 @@
+// GSAP ScrollTrigger 플러그인 등록
+gsap.registerPlugin(ScrollTrigger);
+
 // 페이지 로드 후 실행
 window.addEventListener('DOMContentLoaded', function() {
     
-    // flower2 애니메이션
+    // flower2 스크롤 애니메이션
     gsap.to('.flower2', {
-        x: 50,  // 오른쪽으로 50px 이동
-        y: 30,  // 아래로 30px 이동
-        rotation: 10,  // 10도 회전
-        duration: 3,  // 3초 동안
-        ease: 'power1.inOut',  // 부드러운 움직임
-        repeat: -1,  // 무한 반복
-        yoyo: true  // 왕복 운동
+        rotation: 40,  // 시계방향 40도
+        scrollTrigger: {
+            trigger: '.Welcome',  // 트리거 요소
+            start: 'top bottom',  // Welcome 섹션 상단이 화면 하단에 닿을 때 시작
+            end: 'bottom top',    // Welcome 섹션 하단이 화면 상단에 닿을 때 끝
+            scrub: 1,  // 스크롤과 동기화 (1은 1초 지연)
+            // markers: true  // 디버깅용 (나중에 삭제)
+        }
     });
     
-    // flower3 애니메이션 (nth-child(2))
-    gsap.to('.Welcome img:nth-child(2)', {
-        x: -30,
-        y: 50,
-        rotation: -15,
-        duration: 4,
-        ease: 'power1.inOut',
-        repeat: -1,
-        yoyo: true,
-        delay: 0.5  // 0.5초 후 시작
+    // flower3 스크롤 애니메이션
+    gsap.to('.flower3', {
+        rotation: 40,
+        scrollTrigger: {
+            trigger: '.Welcome',
+            start: 'top bottom',
+            end: 'bottom top',
+            scrub: 1
+        }
     });
 });
